b9d83f873eb1b276724d5ece709cefb6cf45a084
jieunheo <gjekdmlwl@naver.com>
2022-04-27 15:04:53 +0900

    이와같이 문자를 직접적으로 넣는 경우 ts 자체에서 error를 띄움

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -5,5 +5,5 @@ function add(n1: number, n2: number) {
 const number1 = '5';
 const number2 = 2.8;
 
-const result = add(+number1, +number2);
+const result = add(number1, number2);
 console.log(result);
