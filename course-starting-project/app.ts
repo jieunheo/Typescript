ad5f63fb91a93ac6e74e9bd073227d181e64bb5f
jieunheo <gjekdmlwl@naver.com>
2022-04-28 13:19:50 +0900

    타입추론의 예시

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -11,6 +11,14 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 }
 
 const number1 = 5;
+/*
+let number1: number = 5;
+// 이미 값이 5로 숫자형임을 추론할 수 있기 때문에 number를 사용하지 않아도 됌
+
+let number1: number;
+number1 = 5;
+// 이 때 아래의 값에 숫자형이 아닌 값을 넣게 되면 오류를 띄워줌
+*/
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = 'Result is: ';
