7de2122eaecfd3e2a850f11ce4b5953774863c06
jieunheo <gjekdmlwl@naver.com>
2022-04-28 14:31:24 +0900

    enum 타입 사용 방법

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -2,22 +2,37 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; // Tuple
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // Tuple
+// } = {
+//   name: 'hong', // string
+//   age: 30, // number
+//   hobbies: ['Sports', 'Cooking'], // Array
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AURTHOR = 2;
+// -> 각각 따로 관리 해야 하는 겂을 묶어서 관리할 수 있음
+enum Role { ADMIN, READ_ONLY, AURTHOR };
+
+const person = {
   name: 'hong', // string
   age: 30, // number
   hobbies: ['Sports', 'Cooking'], // Array
-  role: [2, 'author']
-}
+  role: Role.ADMIN
+};
 
 // 튜플 - 정해진 갯수의 정해진 타입을 가진 값
 // person.role.push('admin'); // 값 넣기 - 허용
 // person.role.[1] = 10; // 값 바꾸기 - error
 // person.role = []; // 초기화 - error
+
+// 열거 목록 enum
 
 let favoriteActivities: string[];
 // favoriteActivities = 'Sports'; // error
@@ -28,4 +43,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase);
+}
+
+if (person.role === ADMIN) {
+  console.log('is admin');
 }
